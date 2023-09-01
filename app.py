8992ccb3bc4dae357e4a154dd5baec48c39a820e
bean <infoker12@naver.com>
2023-09-01 10:17:08 +0900

    카테고리 설정  upload

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 # -*- coding:utf-8 -*-
 
-import streamlit as st
-import numpy as np
+import streamlit as st 
 
 def main():
+    
+    st.markdown("# Hello World")
+    
+    menu = ['Home', 'EDA', '머신러닝', 'About']
+    choice = st.sidebar.selectbox('메뉴', menu)
 
-    st.markdown('# Hello Wrold')
-    st.write(np.__version__)
+    if choice == 'Home':
+        st.subheader('Home')
+    elif choice == 'EDA':
+        st.subheader('EDA')
+    elif choice == '머신러닝':
+        st.subheader('머신러닝')
+    elif choice == 'About':
+        st.subheader('About')
+    else:
+        pass
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
